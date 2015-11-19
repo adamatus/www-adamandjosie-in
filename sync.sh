6f6e50394dc3376b9a7196ca1e80744b9a681108
Adam Riggall <adam@riggall.info>
2015-11-18 21:05:59 -0800

    Update bucket name

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-aws s3 sync ./build s3://adamandjosie-in/love/ --delete
-aws s3 cp index.html s3://adamandjosie-in/
+aws s3 sync ./build s3://www.adamandjosie.in/love/ --delete
+#aws s3 cp index.html s3://www.adamandjosie.in/
